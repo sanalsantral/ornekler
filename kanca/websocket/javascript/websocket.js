3e7e0886c3909eb0232de2a7c54b8b9218f2fe40
KiPSOFT <newkipsoftware@gmail.com>
2015-09-10 14:56:05 +0300

    websocket kancası javascript örneği.

---
+++ kanca/websocket/javascript/websocket.js
@@ -0,0 +1,17 @@
+var deneme = new WebSocket("wss://api.sanal.link/ws");
+deneme.onopen = function() {
+    var mesaj = {};
+    mesaj["komut"] = "giris";
+    mesaj["apikey"] = "111111111";
+    mesaj["santral_id"] = "200";
+    mesaj["dahili"] = "1007";
+    deneme.send(JSON.stringify(mesaj));
+};
+deneme.onmessage = function(msg) {
+    console.log(decodeURI(msg.data));
+    var mesaj = JSON.parse(decodeURI(msg.data));
+    if (mesaj.komut === "arayan") {
+        console.log("Arayan:"+mesaj.komut);
+        console.log("Tipi:"+mesaj.tipi);
+    }
+};
---
+++ kanca/websocket/readme.md
@@ -0,0 +1,7 @@
+Websocket kancası istediğiniz bir dahiliye gelecek çağrıları yakalamak için kullanılır. 
+
+Ayrıntılı bilgiyi [https://api.sanal.link/#api-Kanca-WebSocketKanca](https://api.sanal.link/#api-Kanca-WebSocketKanca) 
+adresinden alabilirsiniz. 
+ 
+Websocket için origin bilgisinin müşteri panelinden kayıt edilmesi gereklidiri. 
+Her origin için maksimum 10 terminal bağlantısına destek verilir. 
